7ff15ffa6f1c45dc9e1103b9b5b632a5f7263f0e
Ilia Medvedev <ilia.medvedev@codefresh.io>
2024-06-27 19:04:51 +0300

    add sleep

--- v1/.e2e/scripts/init.sh
+++ v1/.e2e/scripts/init.sh
@@ -10,5 +10,6 @@ kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheu
 kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.70.0/example/prometheus-operator-crd-full/monitoring.coreos.com_servicemonitors.yaml
 helm upgrade --install --repo https://charts.loft.sh vcluster vcluster --version 0.16.4 --values $MYDIR/vcluster-values.yaml --values $ROOT_DIR/plugin.yaml --set plugin.prometheus-operator-resources.image=$PLUGIN_IMAGE --wait
 
+sleep 120 # Give time for for CRDs to sync
 
 cat $RESOURCES_FILE | vcluster connect vcluster -n default -- kubectl -n default apply -f -
