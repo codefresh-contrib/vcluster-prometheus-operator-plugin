ed3befc29c88d23f3eb9df03e1db9966aeca2f43
Ilia Medvedev <ilia.medvedev@codefresh.io>
2024-06-27 19:55:39 +0300

    adjust init script and add chainsaw tests

--- v1/.e2e/scripts/init.sh
+++ v1/.e2e/scripts/init.sh
@@ -4,12 +4,29 @@
 PLUGIN_IMAGE=$1
 MYDIR=$(dirname $0)
 ROOT_DIR=$MYDIR/../../
-RESOURCES_FILE=$MYDIR/../vcluster-resources.yaml
+RESOURCES_FILE=$MYDIR/../../../.e2e/vcluster-resources.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.70.0/example/prometheus-operator-crd-full/monitoring.coreos.com_podmonitors.yaml
 kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.70.0/example/prometheus-operator-crd-full/monitoring.coreos.com_servicemonitors.yaml
 helm upgrade --install --repo https://charts.loft.sh vcluster vcluster --version 0.16.4 --values $MYDIR/vcluster-values.yaml --values $ROOT_DIR/plugin.yaml --set plugin.prometheus-operator-resources.image=$PLUGIN_IMAGE --wait
 
-sleep 120 # Give time for for CRDs to sync
+vcluster connect vcluster -n default -- kubectl get servicemonitor && vcluster connect vcluster -n default -- kubectl get podmonitor
+
+res=$?
+secondsWaited=0
+timeout=300
+
+while [ $res -ne 0 ] && [ $secondsWaited -lt $timeout ]; do
+    echo "Waiting for CRDs to get created, sleep for 30 seconds..."
+    sleep 30
+    secondsWaited=$((secondsWaited + 30))
+    vcluster connect vcluster -n default -- kubectl get servicemonitorr && vcluster connect vcluster -n default -- kubectl get podmonitor
+    res=$?
+done
+
+if [ $res -ne 0 ]; then
+  echo "Timed out waiting for CRDs to get created in vcluster"
+  exit 1
+fi
 
 cat $RESOURCES_FILE | vcluster connect vcluster -n default -- kubectl -n default apply -f -
