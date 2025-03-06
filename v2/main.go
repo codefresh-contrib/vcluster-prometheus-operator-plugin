b1d37a5331546d547b0278b76be5586532ce5b7e
Vadim Gusev <ubuntolog@gmail.com>
2025-03-06 18:05:37 +0300

    bump vcluster to v0.23.0

--- v2/main.go
+++ v2/main.go
@@ -4,11 +4,23 @@ import (
 	"github.com/codefresh-contrib/vcluster-prometheus-operator-plugin/v2/syncers/podmonitor"
 	"github.com/codefresh-contrib/vcluster-prometheus-operator-plugin/v2/syncers/servicemonitor"
 	"github.com/loft-sh/vcluster-sdk/plugin"
+	"k8s.io/klog/v2"
 )
 
 func main() {
 	ctx := plugin.MustInit()
-	plugin.MustRegister(servicemonitor.NewServiceMonitorSyncer(ctx))
-	plugin.MustRegister(podmonitor.NewPodMonitorSyncer(ctx))
+
+	serviceMonitorSyncer, err := servicemonitor.NewServiceMonitorSyncer(ctx)
+	if err != nil {
+		klog.Fatalf("new servicemonitor syncer: %v", err)
+	}
+	plugin.MustRegister(serviceMonitorSyncer)
+
+	podMonitorSyncer, err := podmonitor.NewPodMonitorSyncer(ctx)
+	if err != nil {
+		klog.Fatalf("new podmonitor syncer: %v", err)
+	}
+	plugin.MustRegister(podMonitorSyncer)
+
 	plugin.MustStart()
 }
--- v2/syncers/servicemonitor/syncer.go
+++ v2/syncers/servicemonitor/syncer.go
@@ -1,16 +1,21 @@
 package servicemonitor
 
 import (
-	//"context"
+	_ "embed"
+	"fmt"
 
-	internaltranslators "github.com/codefresh-contrib/vcluster-prometheus-operator-plugin/v2/translators"
-	synccontext "github.com/loft-sh/vcluster/pkg/controllers/syncer/context"
-	"github.com/loft-sh/vcluster/pkg/controllers/syncer/translator"
+	"github.com/loft-sh/vcluster/pkg/mappings/generic"
+	"github.com/loft-sh/vcluster/pkg/patcher"
 	"github.com/loft-sh/vcluster/pkg/scheme"
-	synctypes "github.com/loft-sh/vcluster/pkg/types"
+	"github.com/loft-sh/vcluster/pkg/syncer"
+	"github.com/loft-sh/vcluster/pkg/syncer/synccontext"
+	"github.com/loft-sh/vcluster/pkg/syncer/translator"
+	syncertypes "github.com/loft-sh/vcluster/pkg/syncer/types"
+	"github.com/loft-sh/vcluster/pkg/util"
 	"github.com/loft-sh/vcluster/pkg/util/translate"
 	promoperatorv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
-	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -20,52 +25,78 @@ func init() {
 	_ = promoperatorv1.AddToScheme(scheme.Scheme)
 }
 
-func NewServiceMonitorSyncer(ctx *synccontext.RegisterContext) synctypes.Base {
-	return &serviceMonitorSyncer{
-		NamespacedTranslator: translator.NewNamespacedTranslator(ctx, "servicemonitor", &promoperatorv1.ServiceMonitor{}),
+// TODO: rewrite using v2/vendor/k8s.io/apimachinery/pkg/runtime/serializer/json/json.go
+//
+//go:embed crd-servicemonitors.yaml
+var serviceMonitorCRD string
+
+func NewServiceMonitorSyncer(ctx *synccontext.RegisterContext) (syncertypes.Base, error) {
+	CRDManifest := []byte(serviceMonitorCRD)
+	GVK := promoperatorv1.SchemeGroupVersion.WithKind("ServiceMonitor")
+
+	err := util.EnsureCRD(ctx.Context, ctx.PhysicalManager.GetConfig(), CRDManifest, GVK)
+	if err != nil {
+		return nil, err
 	}
+
+	err = util.EnsureCRD(ctx.Context, ctx.VirtualManager.GetConfig(), CRDManifest, GVK)
+	if err != nil {
+		return nil, err
+	}
+
+	mapper, err := generic.NewMapper(ctx, &promoperatorv1.ServiceMonitor{}, translate.Default.HostName)
+	if err != nil {
+		return nil, err
+	}
+
+	return &serviceMonitorSyncer{
+		GenericTranslator: translator.NewGenericTranslator(ctx, "servicemonitor", &promoperatorv1.ServiceMonitor{}, mapper),
+	}, nil
 }
 
 type serviceMonitorSyncer struct {
-	translator.NamespacedTranslator
+	syncertypes.GenericTranslator
 }
 
-var _ synctypes.Initializer = &serviceMonitorSyncer{}
+var _ syncertypes.Syncer = &serviceMonitorSyncer{}
 
-func (s *serviceMonitorSyncer) Init(ctx *synccontext.RegisterContext) error {
-	_, _, err := translate.EnsureCRDFromPhysicalCluster(ctx.Context, ctx.PhysicalManager.GetConfig(), ctx.VirtualManager.GetConfig(), promoperatorv1.SchemeGroupVersion.WithKind("ServiceMonitor"))
-	return err
+func (s *serviceMonitorSyncer) Syncer() syncertypes.Sync[client.Object] {
+	return syncer.ToGenericSyncer(s)
 }
 
-var _ synctypes.Syncer = &serviceMonitorSyncer{}
-
-func (s *serviceMonitorSyncer) SyncToHost(ctx *synccontext.SyncContext, vObj client.Object) (ctrl.Result, error) {
-	return s.SyncToHostCreate(ctx, vObj, s.TranslateMetadata(ctx.Context, vObj).(*promoperatorv1.ServiceMonitor))
+func (s *serviceMonitorSyncer) SyncToHost(ctx *synccontext.SyncContext, event *synccontext.SyncToHostEvent[*promoperatorv1.ServiceMonitor]) (ctrl.Result, error) {
+	pObj := translate.HostMetadata(event.Virtual, s.VirtualToHost(ctx, types.NamespacedName{Name: event.Virtual.Name, Namespace: event.Virtual.Namespace}, event.Virtual))
+	return patcher.CreateHostObject(ctx, event.Virtual, pObj, s.EventRecorder(), true)
 }
 
-func (s *serviceMonitorSyncer) Sync(ctx *synccontext.SyncContext, pObj client.Object, vObj client.Object) (ctrl.Result, error) {
-	return s.SyncToHostUpdate(ctx, vObj, s.translateUpdate(ctx, pObj.(*promoperatorv1.ServiceMonitor), vObj.(*promoperatorv1.ServiceMonitor)))
-}
-
-func (s *serviceMonitorSyncer) translateUpdate(ctx *synccontext.SyncContext, pObj, vObj *promoperatorv1.ServiceMonitor) *promoperatorv1.ServiceMonitor {
-	var updated *promoperatorv1.ServiceMonitor
-
-	// check annotations & labels
-	changed, updatedAnnotations, updatedLabels := s.TranslateMetadataUpdate(ctx.Context, vObj, pObj)
-
-	if changed {
-		updated = translator.NewIfNil(updated, pObj)
-		updated.Labels = updatedLabels
-		updated.Annotations = updatedAnnotations
+func (s *serviceMonitorSyncer) Sync(ctx *synccontext.SyncContext, event *synccontext.SyncEvent[*promoperatorv1.ServiceMonitor]) (_ ctrl.Result, retErr error) {
+	patchHelper, err := patcher.NewSyncerPatcher(ctx, event.Host, event.Virtual)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("new syncer patcher: %w", err)
 	}
 
-	newSpec := internaltranslators.TranslateServiceMonitorSpec(&vObj.Spec, vObj.Namespace)
+	defer func() {
+		if err := patchHelper.Patch(ctx, event.Host, event.Virtual); err != nil {
+			retErr = errors.NewAggregate([]error{retErr, err})
+		}
+		if retErr != nil {
+			s.EventRecorder().Eventf(event.Virtual, "Warning", "SyncError", "Error syncing: %v", retErr)
+		}
+	}()
 
-	// check spec
-	if !equality.Semantic.DeepEqual(newSpec, pObj.Spec) {
-		updated = translator.NewIfNil(updated, pObj)
-		updated.Spec = *newSpec
-	}
+	// any changes made below here are correctly synced
 
-	return updated
+	// sync metadata
+	event.Host.Annotations = translate.HostAnnotations(event.Virtual, event.Host)
+	event.Host.Labels = translate.HostLabels(event.Virtual, event.Host)
+
+	// sync virtual to host
+	event.Host.Spec = event.Virtual.Spec
+
+	return ctrl.Result{}, nil
+}
+
+func (s *serviceMonitorSyncer) SyncToVirtual(ctx *synccontext.SyncContext, event *synccontext.SyncToVirtualEvent[*promoperatorv1.ServiceMonitor]) (ctrl.Result, error) {
+	// virtual object is not here anymore, so we delete
+	return patcher.DeleteHostObject(ctx, event.Host, nil, "virtual object was deleted")
 }
